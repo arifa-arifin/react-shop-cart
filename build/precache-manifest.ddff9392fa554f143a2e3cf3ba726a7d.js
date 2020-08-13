50dd63859368954bc8a7b342d73faf22c1ab633d
Arifa Arifin AHmed <46724873+arifa-arifin@users.noreply.github.com>
2020-08-13 15:12:51 +0530

    Add files via upload

---
+++ build/precache-manifest.ddff9392fa554f143a2e3cf3ba726a7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75ccc8d365ef2e82a7b5c9ef8dc1c917",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db708ff70ecffd1ecb12",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "1d4393bf106bb90340f2",
+    "url": "/static/css/main.1ae6e4cf.chunk.css"
+  },
+  {
+    "revision": "db708ff70ecffd1ecb12",
+    "url": "/static/js/2.70626c02.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.70626c02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d4393bf106bb90340f2",
+    "url": "/static/js/main.77eeaf08.chunk.js"
+  },
+  {
+    "revision": "75b67e50613f7dd4cc1e",
+    "url": "/static/js/runtime-main.5a1d5d0f.js"
+  }
+]);
